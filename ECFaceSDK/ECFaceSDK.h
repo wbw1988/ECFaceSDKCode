044f5a04f721577eda805ca72e599064cfef4497
wangboweniOS <wangbw88@163.com>
2021-08-09 21:56:31 +0800

    20210809

---
+++ ECFaceSDK/ECFaceSDK.h
@@ -0,0 +1,13 @@
+//
+//  ECFaceSDK.h
+//  ECFaceSDK
+//
+//  Created by guojianheng on 2020/7/17.
+//  Copyright © 2020 guojianheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECFaceSDK : NSObject
+
+@end
---
+++ ECFaceSDK/ECFaceSDK.m
@@ -0,0 +1,13 @@
+//
+//  ECFaceSDK.m
+//  ECFaceSDK
+//
+//  Created by guojianheng on 2020/7/17.
+//  Copyright © 2020 guojianheng. All rights reserved.
+//
+
+#import "ECFaceSDK.h"
+
+@implementation ECFaceSDK
+
+@end
